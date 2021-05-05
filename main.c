9d9f6baa1d21bbc758a70fe26d76f9b75b45a92c
Yagdrassyl <andyquan12@gmail.com>
2021-05-04 23:45:00 -0600

    Advancements

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 
+    // Initialize the port
+    int port = 3000;
+
+    // If there is more than one argument,
+    // Assume that a port was passed
+    if (argc > 1)
+    {
+        port = atoi(argv[1]);
+    }
+    printf("Set port as %d\n", port);
     return 0;
 }
